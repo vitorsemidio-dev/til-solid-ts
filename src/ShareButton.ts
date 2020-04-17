0cee56ca3347c029ffc0d48fdd3b0d1ebcf13a30
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-17 00:57:45 -0300

    ShareButton desrespeitando Single Responsability

---
+++ src/ShareButton.ts
@@ -0,0 +1,25 @@
+export default class ShareButton {
+  url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  bind(clazz: string, socialNetwork: string) {
+    let link: string;
+    if (socialNetwork === 'twitter') {
+      link = `https://twitter.com/share?url=${this.url}`;
+    }
+    if (socialNetwork === 'facebook') {
+      link = `https://facebook.com/sharer.php?u=${this.url}`;
+    }
+    if (socialNetwork === 'linkedin') {
+      link = `https://linkedin.com/shareArticle?url=${this.url}`;
+    }
+
+    const elements: any = document.querySelectorAll(clazz);
+    for (const element of elements) {
+      element.addEventListener('click', () => window.open(link));
+    }
+  }
+}
