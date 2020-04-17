88a2eb1a5a32698d0094ba597995703d8ff27bf6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-17 02:07:38 -0300

    Adiçao compartilhamento por impressao. Quebra Liskov Principle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import ShareButtonTwitter from './ShareButtonTwitter';
 import ShareButtonFacebook from './ShareButtonFacebook';
 import ShareButtonLinkedIn from './ShareButtonLinkedIn';
+import ShareButtonPrint from './ShareButtonPrint';
 
 const twitter = new ShareButtonTwitter('.btn-twitter', 'http://www.youtube.com/rodrigobranas');
 twitter.bind();
@@ -8,3 +9,5 @@ const facebook = new ShareButtonFacebook('.btn-facebook', 'http://www.youtube.co
 facebook.bind();
 const linkedin = new ShareButtonLinkedIn('.btn-linkedin', 'http://www.youtube.com/rodrigobranas');
 linkedin.bind();
+const print = new ShareButtonPrint('.btn-print', 'http://www.youtube.com/rodrigobranas');
+print.bind();
